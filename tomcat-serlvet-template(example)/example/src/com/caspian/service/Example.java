ed3d8374e094e28ecfa65476933a31bfe90edab9
rosyCaspian <dyssbest@gmail.com>
2020-11-16 14:48:23 +0800

    tomcat serlvet template

--- tomcat-serlvet-template(example)/example/src/com/caspian/service/Example.java
+++ tomcat-serlvet-template(example)/example/src/com/caspian/service/Example.java
@@ -1,17 +1,15 @@
-package com.caspian.servies;
+package com.caspian.service;
 
 import com.caspian.models.*;
 import com.alibaba.fastjson.JSON;
 import com.caspian.db.*;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
